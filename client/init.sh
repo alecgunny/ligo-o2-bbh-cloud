b6703f13711d0a400d42b89cd201b650ff7c4658
Alec Gunny <alec.gunny@gmail.com>
2021-05-03 14:42:44 -0700

    quieting more install stuff

--- client/init.sh
+++ client/init.sh
@@ -4,9 +4,9 @@ cmd="$1"
 shift
 
 if [[ "$cmd" == "install" ]]; then
-    wget -O $HOME/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    wget -q -O $HOME/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash $HOME/miniconda.sh -b -p $HOME/conda
-    $HOME/conda/bin/conda update -n base -y conda
+    $HOME/conda/bin/conda update -q -n base -y conda
     $HOME/conda/bin/conda init
     rm $HOME/miniconda.sh
 elif [[ "$cmd" == "create" ]]; then
